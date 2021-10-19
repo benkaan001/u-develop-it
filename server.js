e659ada6831f38bde1231767c66cc5f4534b465f
Ben Kaan <benkaan001@gmail.com>
2021-10-19 15:57:39 -0500

    crefactor DELETE candidate

--- server.js
+++ server.js
@@ -32,17 +32,54 @@ console.log('Connected to the election database.')
 //     });
 // });
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
+
+// // GET ALL candidates
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+
+// GET all candidates
+
+app.get('/api/candidates', (req,res) => {
+    const sql = `SELECT * FROM candidates`;
+
+    db.query(sql, (err, rows) => {
+        if(err){
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
 });
+
+// // GET a single candidate
+
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
 
 // GET a single candidate
 
-db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-    if(err){
-        console.log(err);
-    }
-    console.log(row);
+app.get('/api/candidate/:id', (req,res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message});
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
 });
 
 
@@ -55,17 +92,39 @@ db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
 //     console.log(result);
 // });
 
-// CREATE a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-            VALUES (?,?,?,?)`;
-const params =[1, 'Ronald', 'Firbank', 1];
+// // CREATE a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//             VALUES (?,?,?,?)`;
+// const params =[1, 'Ronald', 'Firbank', 1];
 
-db.query(sql, params, (err, result) => {
-    if(err){
-        console.log(err);
-    }
-    console.log(result);
-});
+// db.query(sql, params, (err, result) => {
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// Delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.statusMessage(400).json({ error: res.message });
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Candidate not found'
+        });
+      } else {
+        res.json({
+          message: 'deleted',
+          changes: result.affectedRows,
+          id: req.params.id
+        });
+      }
+    });
+  });
 
 //Default response for any other request(Not Found)
 // Make sure this catchall route comes last in the routes order of appearance
